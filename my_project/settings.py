4fe19e98d061c17cd88ec3b2726001b0cab43a3c
mirek <mirek-maras@tlen.pl>
2020-03-16 16:09:43 +0100

    Django App

--- my_project/settings.py
+++ my_project/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'staticfiles'),
