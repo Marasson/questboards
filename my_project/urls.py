d48827ff72a9cd3f3a0c7bebc87d86e469e61515
mirek <mirek-maras@tlen.pl>
2020-03-16 15:44:06 +0100

    Django App

--- my_project/urls.py
+++ my_project/urls.py
@@ -1,8 +1,3 @@
-#import django.contrib
-#from django.conf.urls import url
-#from django.urls import path, include
-#from django.views.generic.base import TemplateView
-
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include, re_path
