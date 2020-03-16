cea2254a3d3e13bcf8ac954586074c06d092ce53
mirek <mirek-maras@tlen.pl>
2020-03-16 16:14:11 +0100

    Django App

--- my_project/settings.py
+++ my_project/settings.py
@@ -14,7 +14,6 @@ import os
 from django.conf import global_settings
 
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -125,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'staticfiles'),
 )
